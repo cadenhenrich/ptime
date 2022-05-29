ec0fdcaa1cd3ed0aa667fd05cc1271d6a7d2dac2
Caden Henrich <cadenhenrich@gmail.com>
2022-05-29 16:01:37 -0400

    Added basic boilerplate for main tui function

---
+++ src/bin/main.rs
@@ -0,0 +1,27 @@
+use crossterm::{
+    event::{EnableMouseCapture, DisableMouseCapture},
+    terminal::{enable_raw_mode, disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    execute
+};
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // Draw
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
